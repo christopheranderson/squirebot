b0fea49fdb6ffd24e4633f602a7f7e287c9172ce
Chris Anderson (FUNCTIONS) <chrande@microsoft.com>
2017-04-11 12:34:52 -0700

    refactor bot code

---
+++ src/bot/bot.js
@@ -0,0 +1,25 @@
+var builder = require('botbuilder');
+
+// Import dialogs
+const indexDialog = require('./dialogs/index').dialog;
+const handleParametersDialog = require('./dialogs/handleParameters').dialog;
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+
+//=========================================================
+// Bots Dialogs Setup
+//=========================================================
+
+bot.dialog('/', indexDialog);
+
+bot.dialog('/handleParameters', handleParametersDialog).cancelAction("cancelParameters", "Canceled the task, sire", {
+    matches: /^!cancel/i,
+    confirmPrompt: "Are you sure?"
+});
+
+module.exports.connector = connector;
--- src/bot/server.js
+++ src/bot/server.js
@@ -1,8 +1,6 @@
 var restify = require('restify');
-var builder = require('botbuilder');
 
-const indexDialog = require('./dialogs/index').dialog;
-const handleParametersDialog = require('./dialogs/handleParameters').dialog;
+const connector = require('./bot').connector;
 
 //=========================================================
 // Bot Setup
@@ -14,21 +12,5 @@ server.listen(process.env.port || process.env.PORT || 3000, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-var bot = new builder.UniversalBot(connector);
+// Attach connector
 server.post('/api/messages', connector.listen());
-
-//=========================================================
-// Bots Dialogs
-//=========================================================
-
-bot.dialog('/', indexDialog);
-
-bot.dialog('/handleParameters', handleParametersDialog).cancelAction("cancelParameters", "Canceled the task, sire", {
-    matches: /^!cancel/i,
-    confirmPrompt: "Are you sure?"
-});
